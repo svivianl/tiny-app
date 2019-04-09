3c4c37e7e394c7fb8da47ac225eeadd16602d731
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-09 18:29:32 +0000

    add post

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const bodyParser  = require("body-parser");
+var express       = require("express");
+
+
+var app     = express();
+var PORT    = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,10 +14,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
+function generateRandomString() {
+  let data = '1234567890qwertyuiopsdfghjklçazxcvbnm';
+  let code = '';
+
+  data = data.split('');
+  let dataLength = data.length;
+
+  for(let i = 0; i < 6; i++){
+    code += data[Math.floor(Math.random() * Math.floor(dataLength))];
+  }
+
+  return code;
+}
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -22,10 +36,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  // console.log('urls from express: ', templateVars);
-  res.render("urls_index", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -33,6 +49,17 @@ app.get("/urls/:shortURL", (req, res) => {
   // console.log('urlDatabase[req.params.shortURL] ', urlDatabase[req.params.shortURL])
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  // console.log('urls from express: ', templateVars);
+  res.render("urls_index", templateVars);
+});
+
+app.get("/", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
